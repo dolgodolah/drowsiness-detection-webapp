7264692fc1368dd1910c7ce0d30a00b1912de27c
dolgodolah <dolgodolah@gmail.com>
2021-04-21 21:23:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/studyclub/StudyClubApplication.java
@@ -0,0 +1,13 @@
+package com.studyclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyclub/StudyClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
